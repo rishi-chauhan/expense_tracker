c78e76f00acb38ae7d027e005762f24630e7195d
rishi-chauhan <rishi.darkknight.chauhan@gmail.com>
2025-03-17 00:10:52 +0530

    processes pdf to get table data and refactors code

---
+++ expenseTracker/src/components/TableDisplay/TableDisplay.jsx
@@ -0,0 +1,30 @@
+import './TableDisplay.css'
+
+function TableDisplay({ headers, rows }) {
+  if (!headers.length || !rows.length) return null
+
+  return (
+    <div className="table-container">
+      <table>
+        <thead>
+          <tr>
+            {headers.map((header, index) => (
+              <th key={index}>{header}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((cell, cellIndex) => (
+                <td key={cellIndex}>{cell}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default TableDisplay 
